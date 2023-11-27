563758dfe837c3e501b32f2affaa2f223d60e9a0
Angel0880 <angel.cutri@estudiantes.unahur.edu.ar>
2023-11-27 14:00:00 -0300

    Correcciones

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -2,21 +2,32 @@ import React, { useState } from "react";
 import "./taskItem.css";
 
 const TaskItem = ({ tarea, onCompleteTask, onDeleteTask }) => {
-  const { completada, id, title, desc} = tarea;
+  const { completada, id, title, desc } = tarea;
   const [isCompleted, setCompleted] = useState(completada);
+  const [taskDescription, setTaskDescription] = useState(desc);
 
   const handleComplete = () => {
     setCompleted(!isCompleted);
     onCompleteTask(id);
   };
 
+  const handleDescriptionChange = (e) => {
+    setTaskDescription(e.target.value);
+  };
+
   return (
-      <li>
-        <span className={`${isCompleted ? "completado" : ""}`}>{title}</span>
-        <textarea name="desc" id="desc">
-          {desc}
-        </textarea>
-      <button className={`${isCompleted ? "complete" : "no-complete"}`} onClick={handleComplete}>
+    <li>
+      <span className={`${isCompleted ? "completado" : ""}`}>{title}</span>
+      <textarea
+        name="desc"
+        id="desc"
+        value={taskDescription}
+        onChange={handleDescriptionChange}
+      />
+      <button
+        className={`${isCompleted ? "complete" : "no-complete"}`}
+        onClick={handleComplete}
+      >
         {`${isCompleted ? "COMPLETADO" : "NO COMPLETADO"}`}
       </button>
       <button className="delete" onClick={() => onDeleteTask(id)}>
@@ -27,3 +38,4 @@ const TaskItem = ({ tarea, onCompleteTask, onDeleteTask }) => {
 };
 
 export default TaskItem;
+
