e1e7d4689ff1c43b6cc933114220ac1c022fa344
Kevin Agustin Ruiz <kruiz@frba.utn.edu.ar>
2023-11-29 02:15:20 -0300

    estilos responsive v1

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faCheck } from "@fortawesome/free-solid-svg-icons";
-=======
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faCheck  } from '@fortawesome/free-solid-svg-icons';
->>>>>>> 1b3ea80293c7f1c5a5bc9bae5926066b1480f792
 import "./taskItem.css";
 
 const TaskItem = ({ tarea, onCompleteTask, onDeleteTask }) => {
@@ -26,37 +21,24 @@ const TaskItem = ({ tarea, onCompleteTask, onDeleteTask }) => {
     <li>
       <div>
         <h2 className={`${isCompleted ? "completado" : ""}`}>{title}</h2>
-<<<<<<< HEAD
         <textarea
-=======
-        <textarea 
->>>>>>> 1b3ea80293c7f1c5a5bc9bae5926066b1480f792
           name="desc"
           id="desc"
           value={isCompleted ? "" : taskDescription}
           onChange={handleDescriptionChange}
         />
       </div>
-<<<<<<< HEAD
       <div className="iconos">
         <button
           className={`${isCompleted ? "complete" : "no-complete"}`}
           onClick={handleComplete}
         >
-=======
-      <div>
-        <button className={`${isCompleted ? "complete" : "no-complete"}`} onClick={handleComplete}>
->>>>>>> 1b3ea80293c7f1c5a5bc9bae5926066b1480f792
           <FontAwesomeIcon icon={faCheck} />
         </button>
         <button className="delete" onClick={() => onDeleteTask(id)}>
           <FontAwesomeIcon icon={faTrash} />
         </button>
-<<<<<<< HEAD
       </div>
-=======
-      </div>    
->>>>>>> 1b3ea80293c7f1c5a5bc9bae5926066b1480f792
     </li>
   );
 };
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import TaskForm from "./TaskForm/TaskForm";
+import TaskList from "./TaskList/TaskList";
+import TaskItem from "./TaskItem/TaskItem";
+
+export { TaskList, TaskItem, TaskForm };
